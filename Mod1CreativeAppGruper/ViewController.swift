843988828ca7bf07d0333508b8a0cfdfea2701d1
michaelGruper <sarge6411@icloud.com>
2024-09-04 19:49:50 -0500

    Initial Commit

---
+++ Mod1CreativeAppGruper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mod1CreativeAppGruper
+//
+//  Created by Michael Gruper on 9/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
